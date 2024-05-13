8697578ac9c355ab8086ba00c1022bf27620928b
al-amin3009 <abdullah.alamin3009@gmail.com>
2024-05-13 10:56:09 +0600

    first commit

---
+++ src/test/java/PageObjectModel/Checkout.java
@@ -0,0 +1,48 @@
+package PageObjectModel;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import selenium.AbstractComponent;
+
+public class Checkout extends AbstractComponent{
+	WebDriver driver;
+	Actions action;
+	public Checkout(WebDriver driver) {
+		super(driver);
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+		this.action = new Actions(driver);
+	}
+	@FindBy(css="input[placeholder='Select Country']") WebElement countryField;
+	@FindBy(xpath="(//button[contains(@class,'ta-item')])[1]") WebElement selectCountry;
+	@FindBy(className="action__submit") WebElement submit;
+	@FindBy(className="hero-primary") WebElement confirmMessage;
+	@FindBy(css="tr[class='ng-star-inserted'] td[class='em-spacer-1'] label") WebElement orderID;
+	
+	By findBy = By.cssSelector(".ta-results");
+	
+	public void selectCountry(String countryName) {
+		action.sendKeys(countryField, countryName).build().perform();
+		waitForElementAppear(findBy);
+		selectCountry.click();
+	}
+	
+	public void ConfirmOrder() {
+		submit.click();
+	}
+	
+	public String getConfirmMessage() {
+		return confirmMessage.getText();
+	}
+	
+	public String getOrderId() {
+		return orderID.getText();
+	}
+	
+	
+}
---
+++ src/test/java/PageObjectModel/ProductCatalog.java
@@ -0,0 +1,54 @@
+package PageObjectModel;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import selenium.AbstractComponent;
+
+public class ProductCatalog extends AbstractComponent{
+	WebDriver driver;
+	String productTitle;
+	
+	public ProductCatalog(WebDriver driver) {
+		super(driver);
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(css=".mb-3") List<WebElement> products;
+	@FindBy(css="#toast-container") WebElement toastMessage;
+	@FindBy(css=".ng-animating") WebElement loadingAnimator;
+	By productsBy = By.cssSelector(".mb-3");
+	By productTitleBy = By.tagName("h5");
+	By addToCart = By.className("w-10");
+	By toastMessageBy = By.cssSelector("#toast-container");
+	
+	
+	public List<WebElement> getProductList() {
+		waitForElementAppear(productsBy);
+		return products;
+	}
+	
+	
+	public WebElement getProductByName(String productName) {
+		WebElement getProduct = getProductList().stream().filter(product -> product.findElement(productTitleBy).getText().equals(productName))
+				  .findFirst().orElse(null);
+		return getProduct;
+	}
+	
+	
+	public void addProductToCart(String productName) {
+		WebElement getProduct = getProductByName(productName);
+		productTitle = getProduct.findElement(productTitleBy).getText();
+		System.out.println(productTitle);
+		getProduct.findElement(addToCart).click();
+		waitForElementAppear(toastMessageBy);
+		waitForElementDisappear(loadingAnimator);
+	}
+	
+}
